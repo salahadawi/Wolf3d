e4938ff93eeccdb5dd394be6e3ad2092e368da9d
Salah Adawi <sadawi@student.hive.fi>
2020-08-12 20:19:33 +0300

    add sdl struct, SDL2 headers

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wofl3d.h                                           :+:      :+:    :+:   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 16:27:35 by sadawi            #+#    #+#             */
-/*   Updated: 2020/08/12 19:37:41 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/08/12 20:10:38 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WOLF3D_H
 # define WOLF3D_H
 
+# define SCREEN_WIDTH 1600
+# define SCREEN_HEIGHT 900
+
 # include "../libft/includes/libft.h"
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h>
+
+typedef struct			s_sdl
+{
+	SDL_Window			*window;
+	SDL_Surface			*screen;
+	SDL_Surface			*image;
+	SDL_Event			e;
+	int					img_flags;
+	SDL_Renderer		*renderer;
+	//t_texture			*textures;
+}						t_sdl;
 
 #endif
